1d77f1d9497df6cd96c39cee0c0c5eb8586f6c77
Alex Gramm <82889516+Agramm18@users.noreply.github.com>
2024-04-17 18:42:23 +0200

    Add files via upload

---
+++ Adress_Book.py
@@ -0,0 +1,14 @@
+
+
+Dic = {}
+
+Name = input("Geben sie einen Namen an: ")
+Email = str(input("Geben sie eine Email Adresse an: "))
+Tel = int(input("Geben sie eine Telefonnummer an: "))
+
+Dic["Name"] = Name
+Dic["Email"] = Email
+Dic["Tel"] = Tel
+
+print(Dic)
+
